60d127bc567a72d7d69d4cc8f93f2bd6fc051ae8
himanshu007-creator <65963997+himanshu007-creator@users.noreply.github.com>
2020-07-30 14:12:52 +0530

    Add files via upload

---
+++ door mat.py
@@ -0,0 +1,19 @@
+def fun(p,q):
+  x='.|.'
+  a=int((p+1)/2-1)
+  b=int((p+1)/2)
+  c=int(p)
+  for i in range(1,a+1):
+    u=i+(i-1)
+    v=x*u
+    print(v.center(q,'-'))
+  print('WELCOME'.center(q,'-'))
+  for i in range(b+1,c+1):
+    r=((p+1)/2)+1
+    d=int(p-(2*(i-r+1)))
+    e=d*x
+    print(e.center(q,'-'))
+x=list(i for i in input().split(' '))
+n=int(x[0])
+m=int(x[1])
+fun(n,m)
